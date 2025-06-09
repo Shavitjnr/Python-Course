f1e42306b06d17e14cddf43d96cb6ce561a0127d
Shavitjnr <daloutrashavit@gmail.com>
2025-06-09 20:43:17 +0530

    Update Chapter 6

--- Chapter 6/02_if_elif_else_ladder.py
+++ Chapter 6/02_if_elif_else_ladder.py
@@ -2,6 +2,12 @@ a = int(input("Enter a number: "))
 if (a >= 18):
     print("You are eligible for voting")
     print("You have right to do this")
+elif (a < 0):
+    print("You are entering an invalid age")
+    print("Please enter a valid age")
+elif (a == 0):
+    print("You are not eligible for voting")
+    print("You are too young to vote")
 else:
     print("You are not eligible for voting")
     print("You do not have right to do this")
