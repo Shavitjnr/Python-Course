718585faefb32cf62e3b8c09f62ed1f9a51ebfa3
Shavitjnr <daloutrashavit@gmail.com>
2025-06-10 08:57:17 +0530

    Added Chapter 6 Complete

---
+++ Chapter 6/Practice Set/05_pr.py
@@ -0,0 +1,6 @@
+l = ["Shavit","Yeshta","Aman","Anuj"]
+name = input("Enter a name: ")
+if name in l:
+    print("Welcome", name,"!!")
+else:
+    print("Sorry, you are not on the list.")
---
+++ Chapter 6/Practice Set/07_pr.py
@@ -0,0 +1,6 @@
+post = input("Enter a post: ")
+
+if("Shavit".lower() in post.lower()):
+    print("This post is talking about Shavit")
+else:
+    print("This post is not talking about Shavit")
