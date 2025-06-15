4167424f9ac031a9e892201ac7703f25ef6016e9
Shavitjnr <daloutrashavit@gmail.com>
2025-06-16 00:11:12 +0530

    Uploading

--- Project 1/main.py
+++ Project 1/main.py
@@ -6,6 +6,13 @@
 
 
 computer = -1
-you = int(input("Enter your Choice: "))
-youDict = {"s":1,"w":-1,"g":0}
-younum = youDict[you]
+me = int(input("Enter your Choice: "))
+you = {"s":1,"w":-1,"g":0}
+younum = you[me]
+
+
+
+        #  i am tring to make python project, but got an error in the last line. 
+        #  aim is making an snake, water gun game
+        #  if u got the solution, let me know........
+        
